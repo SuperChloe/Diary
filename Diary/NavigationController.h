5e1f13c314a94352289d8cd1c9136728992fb183
Chloe Horgan <super.chloe@gmail.com>
2015-12-31 16:42:14 -0500

    Added NavigationBarController and methods for tinting nav bar

---
+++ Diary/NavigationController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationController.h
+//  Diary
+//
+//  Created by Chloe on 2015-12-31.
+//  Copyright © 2015 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationController : UINavigationController
+
+@end
